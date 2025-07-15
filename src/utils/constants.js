3a2cb7d7a15d7395bd012d5675eba1c1787703c1
Roger101 <roger@centrum.sk>
2025-07-15 14:54:13 +0200

    Server url constant change test 2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // src/utils/constants.js
 
-export const SERVER_URL = 'http://localhost:4000';
+export const SERVER_URL = process.env.REACT_APP_SOCKET_SERVER_URL || 'http://localhost:4000';
 // Môžete tu pridať ďalšie konštanty, napríklad rozmery dosky, atď.
 export const BOARD_SIZE = 15;
 export const RACK_SIZE = 7;
